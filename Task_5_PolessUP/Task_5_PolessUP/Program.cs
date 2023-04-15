a9e0e1f55345b56f836558e3cca87fd8545c888a
AlexeyKlim4yk <114896207+AlexeyKlim4yk@users.noreply.github.com>
2023-04-15 17:52:51 +0300

    Add files via upload

---
+++ Task_5_PolessUP/Task_5_PolessUP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_5_PolessUP
+{
+    class Program
+    { 
+        static int Max(int[] mass)
+        {
+            int max = mass[0];
+            foreach (int x in mass)
+            {
+                if (x > max) max = x;
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            int[] mass = new int[5] { 234, -14, 2, 41, 4 };
+            Console.WriteLine(Max(mass));
+        }
+    }
+}
